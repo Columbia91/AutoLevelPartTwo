b0863ab179c17f76effd48d0f08268cb0a603c2a
Columbia91 <alpha.z91@mail.ru>
2019-04-15 21:38:30 +0600

    Добавьте файлы проекта.

---
+++ AutoLevelPartTwo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Configuration;
+
+namespace AutoLevelPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = ConfigurationManager.ConnectionStrings["appConnection"];
+            var providerName = configuration.ProviderName;
+            var connectionString = configuration.ConnectionString;
+
+            DbProviderFactory providerFactory = DbProviderFactories.GetFactory(providerName);
+            using(var connection = providerFactory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                var dataSet = new DataSet("users");
+                DbDataAdapter dataAdapter = providerFactory.CreateDataAdapter();
+
+                var selectUsersCommand = connection.CreateCommand();
+                selectUsersCommand.CommandText = "select * from Users";
+                dataAdapter.SelectCommand = selectUsersCommand;
+
+                dataAdapter.Fill(dataSet, "Users");
+
+                var commandBuilder = providerFactory.CreateCommandBuilder();
+                commandBuilder.DataAdapter = dataAdapter;
+
+                var usersTable = dataSet.Tables["Users"];
+                var row = usersTable.Rows[0];
+                row.BeginEdit();
+                row["Login"] = "superUser";
+                row.EndEdit();
+
+                dataAdapter.Update(dataSet, "Users");
+            }
+        }
+    }
+}
